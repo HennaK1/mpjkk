66dcdbb5eb09e1af98e0dbf44d570185638c72e5
HennaK1 <henna.koli@metropolia.fi>
2022-03-28 10:39:36 +0300

    logout

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-import PropTypes from 'prop-types';
 import {useContext} from 'react';
 import {Navigate} from 'react-router-dom';
 import {MediaContext} from '../contexts/MediaContext';
 
 const Logout = () => {
-  // eslint-disable-next-line no-unused-vars
   const [user, setUser] = useContext(MediaContext);
   setUser(null);
   localStorage.clear();
